e7f247b0aaba504c885302d19054956f818c3285
Enzo Rodrigues <enzorodrigues272@icloud.com>
2022-11-07 23:24:52 -0300

    Ajustando relacionamentos

--- KeyKing/src/ModelDominio/Agendamento.java
+++ KeyKing/src/ModelDominio/Agendamento.java
@@ -3,24 +3,22 @@ package ModelDominio;
 import java.util.Date;
 
 public class Agendamento {
-    private Periodo periodo;
+    private Agendador agendador; //relacionamento um pra um com agendador
+    private Periodo periodo; //relacionaneto um pra um com periodo
     private Salas salas;
-    private String periodoAgendado;
     private String status;
     private Date dataHoraRegistro;
     private Date dataHoraPego;
     private Date dataHoraEntregue;
     private String observacoes;
 
-    public Agendamento(Salas salas, Date dataHoraEntregue, Date dataHoraPego){
+    public Agendamento(Salas salas, Date dataHoraEntregue, Date dataHoraPego, Agendador agendador, Periodo periodo){
         this.setSalas(salas);
         this.setDataHoraEntregue(dataHoraEntregue);
         this.setDataHoraPego(dataHoraPego);
+        this.setAgendador(agendador);
+        this.setPeriodo(periodo);
     }
-    public String getPeriodoAgendado(){
-        return periodoAgendado;
-    }
-
     public String getStatus() {
         return status;
     }
@@ -41,10 +39,16 @@ public class Agendamento {
         return observacoes;
     }
 
-    public void setPeriodoAgendado(String periodoAgendado) {
-        this.periodoAgendado = periodoAgendado;
+    public Agendador getAgendador() {
+        return agendador;
     }
 
+    public Periodo getPeriodo() {
+        return periodo;
+    }
+    public Salas getSalas(){
+        return salas;
+    }
     public void setStatus(String status) {
         this.status = status;
     }
@@ -64,11 +68,14 @@ public class Agendamento {
     public void setObservacoes(String observacoes) {
         this.observacoes = observacoes;
     }
-    public Salas getSalas(){
-        return salas;
-    }
     public Salas setSalas(Salas salas){
         this.salas = salas;
         return salas;
+    }
+    public void setAgendador(Agendador agendador) {
+        this.agendador = agendador;
+    }
+    public void setPeriodo(Periodo periodo) {
+        this.periodo = periodo;
     }
 }
